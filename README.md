ea410feb7f5a69b2e4062d56bc0bb992ca3d21b0
Josko de Boer <Joskodeboer@gmail.com>
2021-03-23 21:30:50 +0100

    Attempting to parse nc file

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # bayesian-climate
-Bayesian climate modelling
+Download data from http://berkeleyearth.org/data/ (Monthly Land + Ocean, Average Temperature with Air Temperatures at Sea Ice, Equal Area) and put it in ./container/data/dataset.nc
---
+++ container/app.py
@@ -0,0 +1,15 @@
+from matplotlib import pyplot as plt
+import pandas as pd
+import netCDF4
+
+url='/opt/container/data/dataset.nc'
+vname = 'Tx_1211'
+station = 0
+
+nc = netCDF4.Dataset(url)
+print(nc.dimensions['time'].long_name)
+print(nc.variables['temperature'].long_name)
+# h = nc.variables[vname]
+# times = nc.variables['time']
+# jd = netCDF4.num2date(times[:],times.units)
+# hs = pd.Series(h[:,station],index=jd)
