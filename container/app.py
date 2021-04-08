134b525c5c8bb4e1a9f7ab81000aef2a701e1e85
Josko de Boer <joskodeboer@gmail.com>
2021-04-08 22:54:24 +0200

    new dataset, parsed data

--- container/app.py
+++ container/app.py
@@ -3,49 +3,71 @@ import pandas as pd
 import os
 import netCDF4
 import logging
-
+import time
 logger = logging.getLogger()
 logger.setLevel("INFO")
 
-def process_nc_data(path):
-	nc = netCDF4.Dataset(path)
-	nc_dim_time = nc.dimensions['time'] 
+# about the dataset:
+# http://berkeleyearth.org/data/ 
+def toc(tic):
+	toc = time.time()
 
-	nc_var_time = nc.variables['time']
-	nc_var_temperature = nc.variables['temperature']
+	time_diff = toc - tic
 
-	xlabel = nc_dim_time.name
-	ylabel = nc_var_temperature.long_name
-	
-	shape = nc_var_temperature.shape
+	logging.info(f"Time passed is {time_diff} seconds.")
 
-	data_array = []
-
-	xmax = shape[0]
-	ymax = shape[1]
-	#ymax = 100
-	for i in range(0, xmax):
-		x = nc_var_time[i]
-		progress = int(i/shape[0]*1000)/10
-		logging.info(f"Processing data for t={x}, {progress}%")
-		for j in range(0, ymax):
-			y = nc_var_temperature[i,j]
-			data_array.append([x,y])
-
-	
-	return pd.DataFrame(
-		data=data_array,
-		columns=[xlabel, ylabel]
+def read_data(path):
+	df_raw = pd.read_fwf(
+		path,
+		skiprows=86
 	)
-path='/opt/container/data/dataset.nc'
-path_out='/opt/container/data/data_processed.csv'
+	initial_columns = df_raw.columns.values
+	first_row = df_raw.head(1).values.T
 
-if os.path.exists(path_out)  is False:
-	df_data = process_nc_data(path)
-	logging.info(df_data.head())
-	df_data.to_csv(path_out, index=False, compression='gzip')
-	logging.info("Data has been processed.")
-	logging.info(f"See {path_out}")
-else:
-	logging.info("Data has been processed previously.")
-	logging.info(f"See {path_out}")
+	final_columns = {}
+	for i in range(len(initial_columns)):
+		initial = initial_columns[i]
+		row=first_row[i, 0]
+		final_columns[initial] = f"{initial} {row}"
+
+	df_raw = (
+		df_raw
+		.rename(columns=final_columns)
+		.drop(0) #the first row contained some column names, now they're dropped
+	)
+	return df_raw
+ 
+def select_columns(df, column_indices, column_names):
+	"""Returns a dataframe with the selected columns"""
+	df_data = df.copy()
+	columns = df_data.columns.values[column_indices]
+
+	final_columns = {}
+	for i in range(len(columns)):
+		old_name = columns[i]
+		new_name = column_names[i]
+		final_columns[old_name] = new_name
+
+	df_final = df_data[columns].rename(columns=final_columns)
+	
+	return df_final
+# File starts here
+path='/opt/container/data/data.txt' 
+path_out='/opt/container/data/data_processed.csv' 
+
+
+start_time = time.time() 
+toc(start_time)
+
+df_raw = (
+	read_data(path)
+	.pipe(select_columns, [1, 2, 3], ["year", "month", "anomaly"])
+)
+
+print(df_raw.head(10))
+
+toc(start_time)
+
+logging.info("Writing to file")
+df_raw.to_csv(path_out, index=False,)
+toc(start_time)
